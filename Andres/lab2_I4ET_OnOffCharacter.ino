a7f40edd84d50d993c5d2116b9f02b68c3c3dccc
Andres Toala <antoala14@gmail.com>
2025-02-26 16:43:26 +0100

    Add files via upload
    Signed-off-by: Andres Toala <antoala14@gmail.com>

---
+++ Andres/lab2_I4ET_OnOffCharacter.ino
@@ -0,0 +1,28 @@
+byte receivedByte = 0;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  if (Serial.available()>0) // Is there any character coming through the serial port?
+  {
+    //If there is a character, read it
+    receivedByte = Serial.read(); 
+    if (receivedByte == 70)               //If sending ON character "F"
+    {
+      digitalWrite(LED_BUILTIN, LOW);     
+    }
+    if (receivedByte == 78)               //If sending OFF character "N"
+    {
+      digitalWrite(LED_BUILTIN, HIGH);    
+    }
+    //Send the same character back
+    Serial.println(receivedByte);
+  }
+}
